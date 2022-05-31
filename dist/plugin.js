c363bbf330ad1950129b6d8546b83318ea116a54
Todd Miller <todd@zifty.com>
2022-05-30 21:45:20 -0400

    Handle binary sockets in web version

--- dist/plugin.js
+++ dist/plugin.js
@@ -16,28 +16,31 @@ var capacitorNativeWebsocket = (function (exports, core) {
             this.webSocket.onopen = () => {
                 // Connected!
                 console.log('[NWS] Connected');
-                this.notifyListeners('connected', { connected: true });
+                const ret = { connected: true };
+                this.notifyListeners('connected', ret);
             };
             this.webSocket.onmessage = async (msg) => {
                 // Got message
                 console.log('[NWS] Message received', msg);
+                let data = msg.data;
                 if (msg.data instanceof ArrayBuffer) {
-                    this.notifyListeners('message', { data: msg.data, binary: true });
+                    data = base64ArrayBuffer(msg.data);
                 }
-                else {
-                    this.notifyListeners('message', { data: msg.data, binary: false });
-                }
+                const ret = { data: data, binary: (msg.data instanceof ArrayBuffer) };
+                this.notifyListeners('message', ret);
             };
             this.webSocket.onerror = (err) => {
                 // Got error
                 console.log('[NWS] ERROR', err);
                 this.webSocket = null;
-                this.notifyListeners('disconnected', { reason: 'unknown', error: 'unknown' });
+                const ret = { reason: 'PWA Unknown', error: 'PWA Unknown' };
+                this.notifyListeners('disconnected', ret);
             };
             this.webSocket.onclose = (closed) => {
                 console.log('[NWS] Closed: ' + (closed ? closed.code : 'NoCode') + ' ' + (closed ? closed.reason : 'NoReason'));
                 this.webSocket = null;
-                this.notifyListeners('disconnected', { reason: 'disconnected', error: 'disconnected' });
+                const ret = { reason: 'PWA Close', error: 'PWA Close' };
+                this.notifyListeners('disconnected', ret);
             };
         }
         async disconnect() {
@@ -55,6 +58,45 @@ var capacitorNativeWebsocket = (function (exports, core) {
             }
             return { sent: false };
         }
+    }
+    function base64ArrayBuffer(arrayBuffer) {
+        let base64 = '';
+        const encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+        const bytes = new Uint8Array(arrayBuffer);
+        const byteLength = bytes.byteLength;
+        const byteRemainder = byteLength % 3;
+        const mainLength = byteLength - byteRemainder;
+        let a, b, c, d;
+        let chunk;
+        // Main loop deals with bytes in chunks of 3
+        for (let i = 0; i < mainLength; i = i + 3) {
+            // Combine the three bytes into a single integer
+            chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+            // Use bitmasks to extract 6-bit segments from the triplet
+            a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
+            b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
+            c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
+            d = chunk & 63; // 63       = 2^6 - 1
+            // Convert the raw binary segments to the appropriate ASCII encoding
+            base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
+        }
+        // Deal with the remaining bytes and padding
+        if (byteRemainder == 1) {
+            chunk = bytes[mainLength];
+            a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
+            // Set the 4 least significant bits to zero
+            b = (chunk & 3) << 4; // 3   = 2^2 - 1
+            base64 += encodings[a] + encodings[b] + '==';
+        }
+        else if (byteRemainder == 2) {
+            chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+            a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
+            b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
+            // Set the 2 least significant bits to zero
+            c = (chunk & 15) << 2; // 15    = 2^4 - 1
+            base64 += encodings[a] + encodings[b] + encodings[c] + '=';
+        }
+        return base64;
     }
 
     var web = /*#__PURE__*/Object.freeze({
--- src/web.ts
+++ src/web.ts
@@ -1,6 +1,7 @@
 import {WebPlugin} from '@capacitor/core';
 
 import type { NativeWebsocketPlugin } from './definitions';
+import {ConnectedState, DisconnectedState, MessageEvent} from "./definitions";
 
 export class NativeWebsocketWeb
   extends WebPlugin
@@ -15,30 +16,34 @@ export class NativeWebsocketWeb
         this.webSocket.onopen = () => {
             // Connected!
             console.log('[NWS] Connected');
-            this.notifyListeners('connected', { connected: true } );
+            const ret: ConnectedState = { connected: true };
+            this.notifyListeners('connected', ret);
         };
 
         this.webSocket.onmessage = async (msg) => {
             // Got message
             console.log('[NWS] Message received', msg);
+            let data = msg.data;
             if (msg.data instanceof ArrayBuffer) {
-                this.notifyListeners('message', { data: msg.data, binary: true });
-            } else {
-                this.notifyListeners('message', { data: msg.data, binary: false });
+                data = base64ArrayBuffer(msg.data);
             }
+            const ret: MessageEvent = { data: data, binary: (msg.data instanceof ArrayBuffer) };
+            this.notifyListeners('message', ret);
         };
 
         this.webSocket.onerror = (err) => {
             // Got error
             console.log('[NWS] ERROR', err);
             this.webSocket = null;
-            this.notifyListeners('disconnected', { reason: 'unknown', error: 'unknown' } );
+            const ret: DisconnectedState = { reason: 'PWA Unknown', error: 'PWA Unknown' };
+            this.notifyListeners('disconnected', ret);
         };
 
         this.webSocket.onclose = (closed) => {
             console.log('[NWS] Closed: ' + (closed ? closed.code : 'NoCode') + ' ' + (closed ? closed.reason : 'NoReason'));
             this.webSocket = null;
-            this.notifyListeners('disconnected', { reason: 'disconnected', error: 'disconnected' } );
+            const ret: DisconnectedState = { reason: 'PWA Close', error: 'PWA Close' };
+            this.notifyListeners('disconnected', ret);
         };
     }
 
@@ -60,4 +65,56 @@ export class NativeWebsocketWeb
 
         return { sent: false };
     }
+}
+
+function base64ArrayBuffer(arrayBuffer: ArrayBuffer) {
+    let base64    = ''
+    const encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+    const bytes         = new Uint8Array(arrayBuffer)
+    const byteLength    = bytes.byteLength
+    const byteRemainder = byteLength % 3
+    const mainLength    = byteLength - byteRemainder
+
+    let a, b, c, d
+    let chunk
+
+    // Main loop deals with bytes in chunks of 3
+    for (let i = 0; i < mainLength; i = i + 3) {
+        // Combine the three bytes into a single integer
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+        // Use bitmasks to extract 6-bit segments from the triplet
+        a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+        b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
+        c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
+        d = chunk & 63               // 63       = 2^6 - 1
+
+        // Convert the raw binary segments to the appropriate ASCII encoding
+        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+    }
+
+    // Deal with the remaining bytes and padding
+    if (byteRemainder == 1) {
+        chunk = bytes[mainLength]
+
+        a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+        // Set the 4 least significant bits to zero
+        b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+
+        base64 += encodings[a] + encodings[b] + '=='
+    } else if (byteRemainder == 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+        a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+        b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+
+        // Set the 2 least significant bits to zero
+        c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+
+        base64 += encodings[a] + encodings[b] + encodings[c] + '='
+    }
+
+    return base64
 }
