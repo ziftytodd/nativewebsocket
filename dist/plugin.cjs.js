372c3d19605cf404fd542f8da82da97626b0da90
Todd Miller <todd@zifty.com>
2022-05-30 22:00:16 -0400

    Update for ES6 version of buf to base64

--- dist/plugin.cjs.js
+++ dist/plugin.cjs.js
@@ -69,10 +69,13 @@ function base64ArrayBuffer(arrayBuffer) {
     const byteLength = bytes.byteLength;
     const byteRemainder = byteLength % 3;
     const mainLength = byteLength - byteRemainder;
-    let a, b, c, d;
+    let a;
+    let b;
+    let c;
+    let d;
     let chunk;
     // Main loop deals with bytes in chunks of 3
-    for (let i = 0; i < mainLength; i = i + 3) {
+    for (let i = 0; i < mainLength; i += 3) {
         // Combine the three bytes into a single integer
         chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
         // Use bitmasks to extract 6-bit segments from the triplet
@@ -84,20 +87,20 @@ function base64ArrayBuffer(arrayBuffer) {
         base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
     }
     // Deal with the remaining bytes and padding
-    if (byteRemainder == 1) {
+    if (byteRemainder === 1) {
         chunk = bytes[mainLength];
         a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
         // Set the 4 least significant bits to zero
         b = (chunk & 3) << 4; // 3   = 2^2 - 1
-        base64 += encodings[a] + encodings[b] + '==';
+        base64 += `${encodings[a]}${encodings[b]}==`;
     }
-    else if (byteRemainder == 2) {
+    else if (byteRemainder === 2) {
         chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
         a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
         b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
         // Set the 2 least significant bits to zero
         c = (chunk & 15) << 2; // 15    = 2^4 - 1
-        base64 += encodings[a] + encodings[b] + encodings[c] + '=';
+        base64 += `${encodings[a]}${encodings[b]}${encodings[c]}=`;
     }
     return base64;
 }
